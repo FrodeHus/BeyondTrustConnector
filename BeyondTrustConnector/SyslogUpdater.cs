8a1cd494e11da0d274ff89589c08e989fbb59597
frodehus <frode.hus@outlook.com>
2025-07-02 14:17:15 +0200

    Enhance SyslogUpdater to handle exceptions during log parsing and improve code readability

--- BeyondTrustConnector/SyslogUpdater.cs
+++ BeyondTrustConnector/SyslogUpdater.cs
@@ -1,17 +1,24 @@
 using System;
-using BeyondTrustConnector.Parser;
+using System.Globalization;
 using System.IO.Compression;
+using BeyondTrustConnector.Parser;
 using BeyondTrustConnector.Service;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using System.Globalization;
 
 namespace BeyondTrustConnector
 {
-    public class SyslogUpdater(BeyondTrustService beyondTrustService, IngestionService ingestionService, QueryService queryService, ILogger<SyslogUpdater> logger)
+    public class SyslogUpdater(
+        BeyondTrustService beyondTrustService,
+        IngestionService ingestionService,
+        QueryService queryService,
+        ILogger<SyslogUpdater> logger
+    )
     {
         [Function(nameof(SyslogUpdater))]
-        public async Task Run([TimerTrigger("0 */15 * * * *", RunOnStartup = false)] TimerInfo myTimer)
+        public async Task Run(
+            [TimerTrigger("0 */15 * * * *", RunOnStartup = false)] TimerInfo myTimer
+        )
         {
             DateTime? lastEventTime = await GetLastUpdatedTime();
 
@@ -19,30 +26,46 @@ namespace BeyondTrustConnector
             ZipArchive archive = new(new MemoryStream(syslogArchiveData));
             foreach (var entry in archive.Entries)
             {
-                if (entry.Name.EndsWith("gz")) continue;
-
-                logger.LogInformation("Reading {ZipEntry}", entry.Name);
-                await using var entryStream = entry.Open();
-                using var reportReader = new StreamReader(entryStream);
-                var reportContent = await reportReader.ReadToEndAsync();
-                var parser = new BeyondTrustLogParser(reportContent);
-                var events = parser.Parse();
-                if (lastEventTime.HasValue)
+                if (entry.Name.EndsWith("gz"))
+                    continue;
+                try
                 {
-                    events = events.Where(e => e.Timestamp > lastEventTime).ToList();
+                    logger.LogInformation("Reading {ZipEntry}", entry.Name);
+                    await using var entryStream = entry.Open();
+                    using var reportReader = new StreamReader(entryStream);
+                    var reportContent = await reportReader.ReadToEndAsync();
+                    var parser = new BeyondTrustLogParser(reportContent);
+                    var events = parser.Parse();
+                    if (lastEventTime.HasValue)
+                    {
+                        events = events.Where(e => e.Timestamp > lastEventTime).ToList();
+                    }
+
+                    var filteredEvents = events.Where(e =>
+                        e.AdditionalData.ContainsKey("who")
+                        && !e.AdditionalData["who"].StartsWith("Sentinel integration")
+                        && e.EventType != "syslog_report_generated"
+                    );
+                    logger.LogInformation(
+                        "Found {EventCount} total events - filtered to {FilteredEventCount}",
+                        events.Count,
+                        filteredEvents.Count()
+                    );
+                    if (filteredEvents.Any())
+                        await ingestionService.IngestSyslog(filteredEvents.ToList());
                 }
-
-                var filteredEvents = events.Where(e => e.AdditionalData.ContainsKey("who") && !e.AdditionalData["who"].StartsWith("Sentinel integration") && e.EventType != "syslog_report_generated");
-                logger.LogInformation("Found {EventCount} total events - filtered to {FilteredEventCount}", events.Count, filteredEvents.Count());
-                if (filteredEvents.Any())
-                    await ingestionService.IngestSyslog(filteredEvents.ToList());
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Failed to parse syslog entry {ZipEntry}", entry.Name);
+                }
             }
-
         }
 
         private async Task<DateTime?> GetLastUpdatedTime()
         {
-            var result = await queryService.QueryWorkspace("BeyondTrustEvents_CL | summarize arg_max(TimeGenerated,*) | project TimeGenerated");
+            var result = await queryService.QueryWorkspace(
+                "BeyondTrustEvents_CL | summarize arg_max(TimeGenerated,*) | project TimeGenerated"
+            );
             DateTime? lastEventTime = null;
             if (result?.Table.Rows.Count == 1 && result?.Table.Rows[0][0] is not null)
             {
@@ -51,7 +74,10 @@ namespace BeyondTrustConnector
             if (lastEventTime is null)
                 logger.LogWarning("No last updated timestamp found");
             else
-                logger.LogInformation("Last event time: {LastEventTime}", lastEventTime?.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture));
+                logger.LogInformation(
+                    "Last event time: {LastEventTime}",
+                    lastEventTime?.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+                );
 
             return lastEventTime;
         }
